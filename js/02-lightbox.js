993a9e53e9835f555286ae7d3267e73a15a7fed0
Maksym Hanzha <101345604+MaksymHanzha@users.noreply.github.com>
2022-08-15 01:40:08 +0300

    --2_GALLERY--

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// ------------- Розмітка галереї -------------------
+
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+gallery.innerHTML = galleryMarkup;
+var lightbox = new SimpleLightbox(".gallery a", {
+  nav: true,
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+function createGalleryMarkup(gallery) {
+  return gallery
+    .map(
+      ({ preview, original, description }) =>
+        `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a> `
+    )
+    .join("");
+}
